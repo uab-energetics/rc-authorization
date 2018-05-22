91ea73ef78cd9afaa996aab260e2433ed527dba6
Chris Rocco <chris.rocco7@gmail.com>
2018-05-22 13:55:30 -0500

    cleaned up test case

--- test/resource-listener.test.ts
+++ test/resource-listener.test.ts
@@ -1,35 +1,37 @@
 import * as amqplib from 'amqplib'
 import {createRabbitMQConnection, rabbitmqObservable, rabbitmqPublish} from "../src/amqplib-pubsub";
 import {config} from "node-laravel-config/dist";
-import {async} from "rxjs/scheduler/async";
 import {RESOURCE_CREATED_TOPIC, resourceCreatedObservable$} from "../src/resource-listener";
 import {tap} from "rxjs/operators";
 import {loadConfigurationSettings} from "../src/config";
 
-test('Example Test Case', (done) => {
+let ch
+
+beforeEach(async () => {
+    await createRabbitMQConnection(amqplib, {
+        host: config('rabbitHost'),
+        user: config('rabbitUser'),
+        pass: config('rabbitPass')
+    } as any).then( ({ channel }) => ch = channel )
+})
+
+
+test('RabbitMQ Subscription Test Case', (done) => {
 
     loadConfigurationSettings()
 
-    createRabbitMQConnection(amqplib, {
-        host: config('rabbitHost'),
-        user: config('rabbitUser'),
-        pass: config('rabbitPass'),
-    } as any).then( ({channel}) => {
+    rabbitmqObservable(ch, RESOURCE_CREATED_TOPIC)
+        .pipe( tap(console.log) )
+        .subscribe( msg => resourceCreatedObservable$.next(msg) )
 
-        rabbitmqObservable(channel, RESOURCE_CREATED_TOPIC)
-            .pipe( tap(console.log) )
-            .subscribe( msg => resourceCreatedObservable$.next(msg) )
+    setTimeout(() => {
+        // because RabbitMQ will drop fanout messages
+        rabbitmqPublish(ch, { exchange: RESOURCE_CREATED_TOPIC, payload: { msg: 'hello!' } })
+    }, 1000)
 
-        setTimeout(() => {
-            // because RabbitMQ will drop fanout messages
-            rabbitmqPublish(channel, { exchange: RESOURCE_CREATED_TOPIC, payload: { msg: 'hello!' } })
-        }, 1000)
-
-        resourceCreatedObservable$.subscribe( msg => {
-            expect(msg).toBeTruthy()
-            done()
-        })
-
+    resourceCreatedObservable$.subscribe( msg => {
+        expect(msg).toBeTruthy()
+        done()
     })
 
 })
